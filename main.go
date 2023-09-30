026563a8d8ded02019c4a021a95ac2476add3b53
noranorab <noranorab7@gmail.com>
2023-09-30 16:50:38 +0100

    create container from scratch

--- main.go
+++ main.go
@@ -13,6 +13,8 @@ func main() {
 	switch os.Args[1] {
 	case "run":
 		run()
+	case "child":
+		child()
 	default:
 		panic("I am confused")
 	}
@@ -22,13 +24,13 @@ func main() {
 func run() {
 	fmt.Printf("Running %v as user %d in process %d\n", os.Args[2:], os.Geteuid(), os.Getpid())
 
-	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWUSER, //separate hostname from namespace hostname | creating usernamespace for child process
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWUSER | syscall.CLONE_NEWNS | syscall.CLONE_NEWPID, //separate hostname from namespace hostname | creating usernamespace for child process
 		UidMappings: []syscall.SysProcIDMap{
 			{
 				ContainerID: 0,
@@ -45,6 +47,22 @@ func run() {
 		},
 	}
 	must(cmd.Run())
+}
+
+func child() {
+	fmt.Printf("Running %v as user %d in process %d\n", os.Args[2:], os.Geteuid(), os.Getpid())
+
+	must(syscall.Chroot("/home/nora/alpinefs/alpine-minirootfs"))
+	must(os.Chdir("/"))
+	must(syscall.Mount("proc", "proc", "proc", 0, ""))
+
+	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	must(cmd.Run())
+	must(syscall.Unmount("proc", 0))
 }
 
 func must(err error) {
