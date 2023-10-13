030dfcd1ce30ce5c85582902f71ce75b4b2fb3ed
noranorab <noranorab7@gmail.com>
2023-10-13 13:00:55 +0100

    contenurization of app.js

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 	case "run":
 		run()
 	case "child":
-		child()
+		child("app.js")
 	default:
 		panic("I am confused")
 	}
@@ -22,6 +22,31 @@ func main() {
 }
 
 func run() {
+
+	//create a container
+	createContainer()
+
+	//run app in the container
+
+}
+
+func child(appPath string) {
+	fmt.Printf("Running %v as user %d in process %d\n", os.Args[2:], os.Geteuid(), os.Getpid())
+
+	must(syscall.Chroot("/home/nora/Bureau/Perso/os/ubuntu-base-14.04-core-amd64"))
+	must(os.Chdir("/"))
+	must(syscall.Mount("proc", "proc", "proc", 0, ""))
+
+	cmd := exec.Command("/usr/local/node/node-v10.24.1-linux-x64/bin/node", appPath)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	must(cmd.Run())
+	must(syscall.Unmount("proc", 0))
+}
+
+func createContainer() {
 	fmt.Printf("Running %v as user %d in process %d\n", os.Args[2:], os.Geteuid(), os.Getpid())
 
 	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
@@ -47,22 +72,7 @@ func run() {
 		},
 	}
 	must(cmd.Run())
-}
 
-func child() {
-	fmt.Printf("Running %v as user %d in process %d\n", os.Args[2:], os.Geteuid(), os.Getpid())
-
-	must(syscall.Chroot("/home/nora/Bureau/Perso/os/ubuntu-base-14.04-core-amd64"))
-	must(os.Chdir("/"))
-	must(syscall.Mount("proc", "proc", "proc", 0, ""))
-
-	cmd := exec.Command(os.Args[2], os.Args[3:]...)
-	cmd.Stdin = os.Stdin
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-
-	must(cmd.Run())
-	must(syscall.Unmount("proc", 0))
 }
 
 func must(err error) {
